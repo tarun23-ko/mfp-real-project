da67fd2c624512d21dab46b4aa3c3211c2437fac
Tarun Adhikary <bubaiadh@gmail.com>
2023-07-06 01:00:02 +0530

    path single quote-v4

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header'
 export default () => {
     return <BrowserRouter>
             <Header/>
+            <h1>HI</h1>
             <MarketingApp/>
         </BrowserRouter>
 }
