9db7c66e080ee7122b02bb55c51b80ff1274496e
Tarun Adhikary <bubaiadh@gmail.com>
2023-07-05 22:35:53 +0530

    path changed

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import { BrowserRouter } from "react-router-dom";
+import Header from './components/Header'
 export default () => {
-    return <div>
-        <h1>Hi!There</h1>
-        <hr/>
-        <MarketingApp/>
-        </div>
+    return <BrowserRouter>
+            <Header/>
+            <MarketingApp/>
+        </BrowserRouter>
 }
